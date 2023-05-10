4dc1ef26446ec15a4c3cb59c03e3c2504ab58620
Kiraki789 <133057363+Kiraki789@users.noreply.github.com>
2023-05-10 10:30:47 +1000

    Update config.php

--- amo/config.php
+++ amo/config.php
@@ -4,4 +4,4 @@ $client_secret = ''; // Секретный ключ
 $client_id     = ''; // ID интеграции
 $code          = ''; // Код авторизации
 $token_file    = 'tokens.txt';
-$redirect_uri  = 'https://kiraki789.github.io/site2/amo/amo.php';
+$redirect_uri  = 'https://kiraki789.github.io/sitealfa/amo/amo.php';
